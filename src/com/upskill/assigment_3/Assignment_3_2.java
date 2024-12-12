4711d5c12a4d94465355013d3b836820b7944e49
Fahmidaaz <Azfahmida96@gmail.com>
2024-12-12 16:50:02 -0500

    Assignemnt3_1,3_2,3_3 done

--- src/com/upskill/assigment_3/Assignment_3_2.java
+++ src/com/upskill/assigment_3/Assignment_3_2.java
@@ -1,10 +1,47 @@
 package com.upskill.assigment_3;
 
-public class Assignment_3_2 {
+import java.util.Scanner;
 
+//Write a java program which will display 5 employee information(name, age, salary and address).
+
+public class Assignment_3_2 {
+	
+	   String name;
+	   int age;
+	   int salary;
+	   String address;
+	
+	Assignment_3_2(){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter the name of your employee: ");
+		this.name =  s.next();
+		System.out.println("Please enter the age of your employee: ");
+		this.age = s.nextInt();
+		System.out.println("Please enter the salary of your employee: ");
+		this.salary = s.nextInt();
+		System.out.println("Please enter the address of your employee: ");
+		this.address = s.next();
+	}
+    void displayEmployee(){
+    	System.out.println("Name: "+ this.name);
+    	System.out.println("Age: "+ this.age);
+    	System.out.println("Salary: "+ this.salary);
+    	System.out.println("Address: "+ this.address);
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      Assignment_3_2 [] employee = new  Assignment_3_2[5];
+      for(int i =0; i<employee.length; i++){
+    	employee[i] = new Assignment_3_2();
+    	
+      }
+      System.out.println("Displaying 5 employee's information: \t");
+      for(int i=0; i<employee.length; i++){
+    	  System.out.println("\t");
+    	  employee[i].displayEmployee();
+    	 System.out.println("--------------");
+      }
+     
 	}
 
 }
--- src/com/upskill/assigment_3/Assignment_3_3.java
+++ src/com/upskill/assigment_3/Assignment_3_3.java
@@ -1,10 +1,42 @@
 package com.upskill.assigment_3;
 
-public class Assignment_3_3 {
+import java.util.Scanner;
 
+//Bank is a class that provides functionality to get interest rate. 
+//Display different interest rate using rate functionality for Chase, City and TD banks followed by 8%, 7% and 9%. Hints: using method override.
+
+public class Assignment_3_3 {
+	
+	
+	  void displayInterestRate(){
+		  System.out.println("Nothing to show. Select your bank name");
+	  }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Assignment_3_3 bank = new ChaseBank();
+        bank.displayInterestRate();
+        new CityBank().displayInterestRate();
+        new TDBank().displayInterestRate();
+        
+        
+        
 	}
 
+}
+
+class ChaseBank extends Assignment_3_3{
+	void displayInterestRate(){
+		System.out.println("Interest rate of chase bank is 8%");
+	}
+}
+class CityBank extends Assignment_3_3{
+	void displayInterestRate(){
+		System.out.println("Interest rate of city bank is 7%");
+	}
+}
+class TDBank extends Assignment_3_3{
+	void displayInterestRate(){
+		System.out.println("Interest rate of TD bank is 9%");
+	}
 }
